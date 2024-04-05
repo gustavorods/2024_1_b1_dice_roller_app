308de012de670687fce8869cc4a5506b3fe55433
gustavorods <gustavo.rlsilva07@gmail.com>
2024-04-05 16:24:07 -0300

    chore: toats instead of text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,6 +16,10 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.LocalContext
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -37,6 +41,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -44,6 +49,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import com.google.ads.interactivemedia.pal.utils.Duration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +81,8 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf( 1) }
     var chuteDoUsuario by remember { mutableStateOf("") }
     var ResultadoDaAposta by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
 
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
@@ -90,6 +98,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         Button(
             onClick = { result = (1..6).random()
                         ResultadoDaAposta = verificacaoDaAposta(chuteDoUsuario.toInt(), result)
+                        Toast.makeText(context, ResultadoDaAposta, Toast.LENGTH_SHORT).show()
             },
         ) {
             Text(text = stringResource(R.string.roll), fontSize = 24.sp)
@@ -102,10 +111,6 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
-        Text(
-            text = ResultadoDaAposta,
-            modifier = modifier
-        )
     }
 }
 
